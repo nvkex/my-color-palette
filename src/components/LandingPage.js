5a8330797f6096bc9c800e65a9ae52f162a92643
nvkex <scrbwrm@gmail.com>
2020-09-08 21:01:36 +0530

    Landing Page

---
+++ src/components/LandingPage.js
@@ -0,0 +1,172 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+
+
+export default function LandingPage() {
+  return (
+    <div>
+      <Navbar />
+
+      <div className="main-grid">
+
+        <a className="pallete-card shadow" href="/">
+          <div className="pallete-sm">
+            <div className="pallete-sm-item" style={{ backgroundColor: '#1abc9c' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#2ecc71' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#3498db' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#9b59b6' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#34495e' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#16a085' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#27ae60' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#2980b9' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#8e44ad' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#2c3e50' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#f1c40f' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#e67e22' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#e74c3c' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#ecf0f1' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#95a5a6' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#f39c12' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#d35400' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#c0392b' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#bdc3c7' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#7f8c8d' }}></div>
+          </div>
+          <span className="pallete-desc">Flat UI</span>
+        </a>
+
+        <a className="pallete-card shadow" href="/">
+          <div className="pallete-sm">
+            <div className="pallete-sm-item" style={{ backgroundColor: '#1abc9c' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#2ecc71' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#3498db' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#9b59b6' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#34495e' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#16a085' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#27ae60' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#2980b9' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#8e44ad' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#2c3e50' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#f1c40f' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#e67e22' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#e74c3c' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#ecf0f1' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#95a5a6' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#f39c12' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#d35400' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#c0392b' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#bdc3c7' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#7f8c8d' }}></div>
+          </div>
+          <span className="pallete-desc">Flat UI</span>
+        </a>
+
+        <a className="pallete-card shadow" href="/">
+          <div className="pallete-sm">
+            <div className="pallete-sm-item" style={{ backgroundColor: '#1abc9c' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#2ecc71' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#3498db' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#9b59b6' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#34495e' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#16a085' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#27ae60' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#2980b9' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#8e44ad' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#2c3e50' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#f1c40f' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#e67e22' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#e74c3c' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#ecf0f1' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#95a5a6' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#f39c12' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#d35400' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#c0392b' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#bdc3c7' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#7f8c8d' }}></div>
+          </div>
+          <span className="pallete-desc">Flat UI</span>
+        </a>
+
+        <a className="pallete-card shadow" href="/">
+          <div className="pallete-sm">
+            <div className="pallete-sm-item" style={{ backgroundColor: '#1abc9c' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#2ecc71' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#3498db' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#9b59b6' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#34495e' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#16a085' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#27ae60' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#2980b9' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#8e44ad' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#2c3e50' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#f1c40f' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#e67e22' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#e74c3c' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#ecf0f1' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#95a5a6' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#f39c12' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#d35400' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#c0392b' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#bdc3c7' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#7f8c8d' }}></div>
+          </div>
+          <span className="pallete-desc">Flat UI</span>
+        </a>
+
+        <a className="pallete-card shadow" href="/">
+          <div className="pallete-sm">
+            <div className="pallete-sm-item" style={{ backgroundColor: '#1abc9c' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#2ecc71' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#3498db' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#9b59b6' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#34495e' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#16a085' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#27ae60' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#2980b9' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#8e44ad' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#2c3e50' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#f1c40f' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#e67e22' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#e74c3c' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#ecf0f1' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#95a5a6' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#f39c12' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#d35400' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#c0392b' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#bdc3c7' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#7f8c8d' }}></div>
+          </div>
+          <span className="pallete-desc">Flat UI</span>
+        </a>
+
+        <a className="pallete-card shadow" href="/">
+          <div className="pallete-sm">
+            <div className="pallete-sm-item" style={{ backgroundColor: '#1abc9c' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#2ecc71' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#3498db' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#9b59b6' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#34495e' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#16a085' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#27ae60' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#2980b9' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#8e44ad' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#2c3e50' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#f1c40f' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#e67e22' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#e74c3c' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#ecf0f1' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#95a5a6' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#f39c12' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#d35400' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#c0392b' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#bdc3c7' }}></div>
+            <div className="pallete-sm-item" style={{ backgroundColor: '#7f8c8d' }}></div>
+          </div>
+          <span className="pallete-desc">Flat UI</span>
+        </a>
+
+      </div>
+    </div>
+  );
+}
