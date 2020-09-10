035ab439c63b92053814274713b1f786898c5540
nvkex <scrbwrm@gmail.com>
2020-09-10 21:28:56 +0530

    Mounting all components to App

--- src/App.js
+++ src/App.js
@@ -4,14 +4,23 @@ import { Switch, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import SignUp from './components/SignUp';
 import Login from './components/Login';
+import SinglePalette from './components/SinglePalette';
+import Navbar from './components/Navbar';
 
-function App() {
+function App(props) {
   return (
-    <Switch>
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/signup" component={SignUp} />
-      <Route path="/" component={LandingPage}/>
-    </Switch>
+    <div>
+      <Navbar history={props.history} />
+      <div>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+          <Route path="/palette/:id" component={SinglePalette} />
+          <Route path="/" component={LandingPage} />
+        </Switch>
+      </div>
+    </div>
+
   );
 }
 
