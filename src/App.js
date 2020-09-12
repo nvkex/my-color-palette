cf0ea1a3d6baba8e55d03162f427765143ecbcae
nvkex <scrbwrm@gmail.com>
2020-09-12 15:57:02 +0530

    login user automatically

--- src/App.js
+++ src/App.js
@@ -6,14 +6,30 @@ import SignUp from './components/Forms/Signup';
 import Login from './components/Forms/Login';
 import SinglePalette from './components/SinglePalette';
 import Navbar from './components/Navbar';
+import Dashboard from './components/User/Dashboard';
+import Logout from './components/User/Logout';
+import { useDispatch } from 'react-redux';
+import { loginUser } from './actions/AuthActions';
 
 function App(props) {
+
+  const token = localStorage.getItem('token');
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  const dispatch = useDispatch();
+
+  if(token && user){
+    dispatch(loginUser(200, user, token));
+  }
+
   return (
     <div>
       <Navbar history={props.history} />
       <div>
         <Switch>
+          <Route exact path="/dashboard" component={Dashboard}/>
           <Route exact path="/login" component={Login} />
+          <Route exact path="/logout" component={Logout} />
           <Route exact path="/signup" component={SignUp} />
           <Route path="/palette/:id" component={SinglePalette} />
           <Route path="/" component={LandingPage} />
