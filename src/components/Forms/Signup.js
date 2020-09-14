cf4e3bd2ceb7696b4a9e318ef67060a0a49efa78
nvkex <scrbwrm@gmail.com>
2020-09-14 22:38:17 +0530

    feat: validate and register a new user to db

--- src/components/Forms/Signup.js
+++ src/components/Forms/Signup.js
@@ -1,25 +1,96 @@
+import axios from 'axios';
 import React from 'react'
+import { withRouter } from 'react-router';
 
-const Signup = () => {
+const formHandler = async (props, e) => {
+
+    const URL = "https://my-color-palette.herokuapp.com";
+    e.preventDefault();
+
+    var emailExists = false;
+
+    const loader = document.querySelector('.lds-ring');
+    const name = document.querySelector('#name').value;
+    const email = document.querySelector('#email').value;
+    const password = document.querySelector('#pass').value;
+    const cnfPassword = document.querySelector('#cnfPass').value;
+    const formData = { name, email, password };
+
+    if (!password || !cnfPassword || !email || !name) {
+        alert('Fields cannot be empty!');
+        return;
+    }
+
+    // Check if confirm password matches with password
+    if (password !== cnfPassword) {
+        alert('Password did not match!');
+        return;
+    }
+
+    loader.style.display = 'inline-block';
+
+    // Check if email exists
+    await axios.get(`${URL}/auth/check-email?email=${email}`)
+        .then(res => {
+            emailExists = res.data.exists;
+        })
+        .catch(err => {
+            console.log(err);
+        });
+
+    if (emailExists) {
+        alert('Email already exists!');
+        loader.style.display = 'none';
+        return;
+    }
+
+    // All good, user can now register.
+    axios.post(`${URL}/auth/signup`, formData)
+        .then(res => {
+            if (res.data.status === 200) {
+                props.history.push('/login');
+            }
+            else
+                alert('Error occured! Try again later.');
+        })
+        .catch(err => {
+            console.log(err);
+        })
+
+        loader.style.display = 'none';
+
+}
+
+
+const Signup = (props) => {
+
+
     return (
         <div className="signup">
             <form className="shadow-lg" action="/">
-                <h3 className="text-center">Join Us!</h3>
-                <label>Email</label>
-                <input type="text" name="email" placeholder="" />
-                <br />
-                <label>Password</label>
-                <input type="text" name="password" placeholder="" />
-                <br />
-                <label>Confirn Password</label>
-                <input type="text" name="cnfPass" placeholder="" />
-                
+                <h3 className="text-center">
+                    Join Us!
+                </h3>
+                <label>Name</label>
+                <input type="text" name="name" id="name" placeholder="" required />
                 <label></label>
-                <br />
-                <input type="submit" value="Signup" />
+                <label>Email</label>
+                <input type="email" name="email" id="email" placeholder="" required />
+                <label></label>
+                <label>Password</label>
+                <input type="password" name="password" id="pass" placeholder="" required />
+                <label></label>
+                <label>Confirm Password</label>
+                <input type="password" name="cnfPass" id="cnfPass" placeholder="" required />
+
+                <label></label>
+                <label></label>
+                <input type="submit" value="Signup" onClick={(e) => formHandler(props, e)} />
+                <label></label>
+                <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
             </form>
         </div>
     )
 }
 
-export default Signup
+export default withRouter(Signup);
