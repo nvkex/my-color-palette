5b22b178f5b096fad45767d3223f78c89511ee96
nvkex <scrbwrm@gmail.com>
2020-09-15 18:13:02 +0530

    Dashboard template update

--- src/components/User/Dashboard.js
+++ src/components/User/Dashboard.js
@@ -7,8 +7,44 @@ export default function Dashboard() {
   const { token, user } = auth;
   console.log(token, user);
   return (
-    <div>
-        Dashboard
+    <div className="dashboard">
+
+      <div className="profile-section shadow">
+        <img src="./static/default_profile.png" alt="profile_picture" />
+        <div>
+          <span>John Doe</span>
+        </div>
+        <div>
+          <span>johndoe@xyz.com</span>
+        </div>
+      </div>
+
+      <div className="user-palette-detail shadow">
+
+        <div>
+          <div className="text-center heading">Custom Palettes</div>
+          <hr />
+          <p>
+            <span className="">Count</span> : <span> 4 </span>
+          </p>
+
+          <button>View all</button>
+
+        </div>
+
+        <div>
+          <div className="text-center heading">Favorite Palettes</div>
+          <hr />
+          <p>
+            <span className="">Count</span> : <span> 3 </span>
+          </p>
+
+          <button>View all</button>
+
+        </div>
+
+      </div>
+
     </div>
   );
 }
