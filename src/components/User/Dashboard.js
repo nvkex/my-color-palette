813e619c978a38648bf354aa0c1b418e63d939bd
nvkex <scrbwrm@gmail.com>
2020-09-16 10:52:19 +0530

    update: dashboard design and layout

--- src/components/User/Dashboard.js
+++ src/components/User/Dashboard.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Redirect } from 'react-router';
+import { Link } from 'react-router-dom';
 
 export default function Dashboard() {
 
   const auth = useSelector(state => state.authReducer);
   const { token, user } = auth;
-  console.log(token, user);
+
+  if (!token || !user) {
+    return <Redirect to="/login" />
+  }
+
   return (
     <div className="dashboard">
 
       <div className="profile-section shadow">
         <img src="./static/default_profile.png" alt="profile_picture" />
         <div>
-          <span>John Doe</span>
+          <span>{user.name}</span>
         </div>
         <div>
-          <span>johndoe@xyz.com</span>
+          <span>{user.email}</span>
         </div>
       </div>
 
@@ -25,10 +31,30 @@ export default function Dashboard() {
           <div className="text-center heading">Custom Palettes</div>
           <hr />
           <p>
-            <span className="">Count</span> : <span> 4 </span>
+            Total Palettes
+          </p>
+          <p className="text-muted">
+            7
           </p>
 
-          <button>View all</button>
+          <Link to="/my-palettes">
+            <button>
+              <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-eyeglasses" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M4 6a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm2.625.547a3 3 0 0 0-5.584.953H.5a.5.5 0 0 0 0 1h.541A3 3 0 0 0 7 8a1 1 0 0 1 2 0 3 3 0 0 0 5.959.5h.541a.5.5 0 0 0 0-1h-.541a3 3 0 0 0-5.584-.953A1.993 1.993 0 0 0 8 6c-.532 0-1.016.208-1.375.547zM14 8a2 2 0 1 0-4 0 2 2 0 0 0 4 0z" />
+              </svg>
+              &nbsp; View all
+            </button>
+          </Link>
+          <br />
+          <Link to="/">
+            <button>
+              <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-plus-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                <path fill-rule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+              </svg>
+              &nbsp; New Palette
+            </button>
+          </Link>
 
         </div>
 
@@ -36,10 +62,30 @@ export default function Dashboard() {
           <div className="text-center heading">Favorite Palettes</div>
           <hr />
           <p>
-            <span className="">Count</span> : <span> 3 </span>
+            Total Palettes
+          </p>
+          <p className="text-muted">
+            4
           </p>
 
-          <button>View all</button>
+          <Link to="/favorites">
+            <button>
+              <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-eyeglasses" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M4 6a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm2.625.547a3 3 0 0 0-5.584.953H.5a.5.5 0 0 0 0 1h.541A3 3 0 0 0 7 8a1 1 0 0 1 2 0 3 3 0 0 0 5.959.5h.541a.5.5 0 0 0 0-1h-.541a3 3 0 0 0-5.584-.953A1.993 1.993 0 0 0 8 6c-.532 0-1.016.208-1.375.547zM14 8a2 2 0 1 0-4 0 2 2 0 0 0 4 0z" />
+              </svg>
+              &nbsp; View all
+            </button>
+          </Link>
+          <br />
+          <Link to="/explore">
+            <button>
+              <svg width="1em" height="1.0625em" viewBox="0 0 16 17" class="bi bi-compass" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M8 16.016a7.5 7.5 0 0 0 1.962-14.74A1 1 0 0 0 9 0H7a1 1 0 0 0-.962 1.276A7.5 7.5 0 0 0 8 16.016zm6.5-7.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z" />
+                <path d="M6.94 7.44l4.95-2.83-2.83 4.95-4.949 2.83 2.828-4.95z" />
+              </svg>
+              &nbsp; Explore Palettes
+            </button>
+          </Link>
 
         </div>
 
