cdccb15a06805f365d3ddf0f9c2823e5c0827ff8
nvkex <scrbwrm@gmail.com>
2020-09-17 12:26:18 +0530

    Comments and function descriptions

--- src/actions/AuthActions.js
+++ src/actions/AuthActions.js
@@ -3,11 +3,19 @@ import {
     LOGOUT
 } from '../constants/AuthConstants';
 
+/**
+ * Logs in a user, storing their information and JWT token in redux store
+ * @param {Number} status - status of the login API request
+ * @param {Object} userData - All information about the user trying to login
+ * @param {String} token - JWT token sent by the API
+ */
 const loginUser = (status, userData, token) => async (dispatch) => {
     if (status === 200)
         dispatch({ type: LOGIN, payload: {userData, token} })
 }
-
+/**
+ * Logs out the user
+ */
 const logoutUser = () => async (dispatch) => {
     dispatch({ type: LOGOUT })
 }
