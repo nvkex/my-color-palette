6bb178d9d083b47ec61c8f80fac230c33cb0fed5
nvkex <scrbwrm@gmail.com>
2020-09-19 10:15:33 +0530

    check token expiry & email routes

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,9 +4,13 @@ const router = express.Router();
 const { 
     signupController,
     loginController,
-    verifyToken
+    verifyToken,
+    checkEmail,
+    checkTokenExpiry
 } = require('../controllers/auth');
 
+router.get('/check-token-expiry', checkTokenExpiry);
+router.get('/check-email', checkEmail);
 router.post('/signup', signupController);
 router.post('/login', loginController);
 router.post('/verify',verifyToken);
