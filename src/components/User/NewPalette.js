2654bb6089d3af46367c8026ad3502da06957ef4
nvkex <scrbwrm@gmail.com>
2020-09-25 11:27:02 +0530

    display modal instead of alert box

--- src/components/User/NewPalette.js
+++ src/components/User/NewPalette.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import axios from 'axios';
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
@@ -13,8 +14,8 @@ export default function NewPalette() {
     return <Redirect to="/login" />
   }
 
-  // eslint-disable-next-line react-hooks/rules-of-hooks
   const [colors, setColors] = useState([]);
+  const [modalData, setModalData] = useState({ display: false });
 
   /**
    * Add the color in input box to the state and therefore to the palette.
@@ -72,21 +73,25 @@ export default function NewPalette() {
   const handleCreatePalette = (e) => {
     e.preventDefault();
 
+
+
     const title = document.querySelector('#title').value;
     const author = {
       id: user._id,
       name: user.name
     }
 
-    if (colors.length >= 20 || colors.length === 0) {
-      alert('Please add atleast one and max 20 colors to the palette');
+
+    if (!title) {
+      setModalData({ display: true, head: "Error!", desc: "Title is required!", color: "danger" });
       return;
     }
 
-    if (!title) {
-      alert('Title is required!');
+    if (colors.length >= 20 || colors.length === 0) {
+      setModalData({ display: true, head: "Error!", desc: "Please add max 20 and min 1 color.", color: "danger" })
       return;
     }
+
 
     // show spinner
     const spinner = document.querySelector('.spinner');
@@ -108,10 +113,10 @@ export default function NewPalette() {
           spinner.style.display = 'none';
           // Creation successfull when res.data.success is true
           if (res.data.success)
-            alert('Success!');
+            setModalData({ display: true, head: "Success!", desc: "Palette Saved.", color: "success" });
           else
-            alert('Failed!');
-          
+            setModalData({ display: true, head: "Failed!", desc: "An error occured, palette wasn't saved.", color: "danger" });
+
         })
         .catch(err => {
           console.log(err);
@@ -125,80 +130,80 @@ export default function NewPalette() {
 
   return (
     <div>
-      <Modal />
-    <div className="new-palette-container shadow-lg">
-      <h1 className=" text-center text-dark">Create new palette</h1>
+      <Modal data={modalData} setDisplay={setModalData} />
+      <div className="new-palette-container shadow-lg">
+        <h1 className=" text-center text-dark">Create new palette</h1>
 
-      <div className="form-container">
-        <form>
-          <input type="text" placeholder="Title" id="title" required />
-          <label></label>
-          <label></label>
-          <span>
-            <input type="text" placeholder="Color hex" id="colorHex" onChange={handleColorPreview} />
-            <button id="#addColor" onClick={(e) => handleAddColor(e)}>Add</button>
-          </span>
+        <div className="form-container">
+          <form>
+            <input type="text" placeholder="Title" id="title" required />
+            <label></label>
+            <label></label>
+            <span>
+              <input type="text" placeholder="Color hex" id="colorHex" onChange={handleColorPreview} />
+              <button id="#addColor" onClick={(e) => handleAddColor(e)}>Add</button>
+            </span>
 
-          <label></label>
-          <label></label>
+            <label></label>
+            <label></label>
 
-          <div className="new-color-preview text-center"></div>
+            <div className="new-color-preview text-center"></div>
 
-          <label></label>
-          <label></label>
-          <label className="text-danger">*Max 20 colors are allowed per palette.</label>
-          <label></label>
-          <label></label>
+            <label></label>
+            <label></label>
+            <label className="text-danger">*Max 20 colors are allowed per palette.</label>
+            <label></label>
+            <label></label>
 
-          <div className="action-btns">
-            <button id="createBtn" onClick={(e) => handleCreatePalette(e)}>Create</button>
-            <button id="cancelBtn" onClick={(e) => handleClearColors(e)}>Clear</button>
-          </div>
+            <div className="action-btns">
+              <button id="createBtn" onClick={(e) => handleCreatePalette(e)}>Create</button>
+              <button id="cancelBtn" onClick={(e) => handleClearColors(e)}>Clear</button>
+            </div>
 
-          <div className="spinner text-muted">
-            <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
-            <p>Saving...</p>
-          </div>
+            <div className="spinner text-muted">
+              <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+              <p>Saving...</p>
+            </div>
 
 
-        </form>
+          </form>
 
-        <div className="color-preview-section">
+          <div className="color-preview-section">
 
-          <div className="pallete-card border" >
-            <div className="pallete-sm">
+            <div className="pallete-card border" >
+              <div className="pallete-sm">
+                {
+                  colors.length === 0 ?
+                    (<p className="text-muted">Empty</p>)
+                    :
+                    colors.map((color, index) => (
+                      <div key={index} className="pallete-sm-item" style={{ backgroundColor: color }} ></div>
+                    ))
+                }
+              </div>
+            </div>
+
+            <div className="color-code-list">
+
               {
                 colors.length === 0 ?
-                  (<p className="text-muted">Empty</p>)
+                  null
                   :
                   colors.map((color, index) => (
-                    <div key={index} className="pallete-sm-item" style={{ backgroundColor: color }} ></div>
+                    <div className="color-code-container" key={index}>
+                      <span className="color-code">{color}</span> &nbsp;
+                      <button id="delColor" onClick={(e) => handleDelColor(e, color)}>x</button>
+                    </div>
                   ))
               }
+
             </div>
-          </div>
-
-          <div className="color-code-list">
-
-            {
-              colors.length === 0 ?
-                null
-                :
-                colors.map((color, index) => (
-                  <div className="color-code-container" key={index}>
-                    <span className="color-code">{color}</span> &nbsp;
-                    <button id="delColor" onClick={(e) => handleDelColor(e, color)}>x</button>
-                  </div>
-                ))
-            }
 
           </div>
-
         </div>
+
+
       </div>
-
-
-    </div>
     </div>
   );
 }
