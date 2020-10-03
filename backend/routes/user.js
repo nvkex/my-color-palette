3ffa11f15553951a590b91026f60aa5a79fe9015
nvkex <scrbwrm@gmail.com>
2020-10-03 20:10:14 +0530

    add:upvote route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 
+const { verifyToken, checkExpiry } = require('../middlewares/auth.middleware');
+
 const {
     getUserPalettes,
     createNewPalette,
-    deletePalette
+    deletePalette,
+    upvotePalette
 } = require('../controllers/user');
 
 router.post('/new-palette', createNewPalette);
@@ -11,5 +14,7 @@ router.post('/new-palette', createNewPalette);
 router.post('/delete', deletePalette);
 
 router.post('/user-palettes', getUserPalettes);
+
+router.post('/upvote', [verifyToken, checkExpiry], upvotePalette);
 
 module.exports = router;
